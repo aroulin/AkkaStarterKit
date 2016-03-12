67c8778051a906c71ab0e9905e8fab0b1943de31
Andy Roulin <andy@cyberhaven.io>
2016-03-12 05:52:23 +0100

    Better better track detection

--- src/main/java/com/zuehlke/carrera/javapilot/akka/PowerUpUntilPenalty.java
+++ src/main/java/com/zuehlke/carrera/javapilot/akka/PowerUpUntilPenalty.java
@@ -12,8 +12,8 @@ import org.apache.commons.lang.StringUtils;
 import java.util.ArrayList;
 
 /**
- *  this logic node increases the power level by 10 units per 0.5 second until it receives a penalty
- *  then reduces by ten units.
+ * this logic node increases the power level by 10 units per 0.5 second until it receives a penalty
+ * then reduces by ten units.
  */
 public class PowerUpUntilPenalty extends UntypedActor {
 
@@ -21,16 +21,9 @@ public class PowerUpUntilPenalty extends UntypedActor {
 
     private double currentPower = 0;
 
-    private long lastIncrease = 0;
-
-    private final int constantPower = 110;
-
     private SECTION currentSection = SECTION.STILL_STANDING;
 
-    private ArrayList<SECTION> track = new ArrayList<SECTION>(), compareTrack = new ArrayList<SECTION>();
-    private ArrayList<SECTION> checkTrack = new ArrayList<SECTION>();
-
-    private int indexCompareTrack = 0;
+    private ArrayList<SECTION> track = new ArrayList<>(), compareTrack = new ArrayList<>();
 
     private boolean newSection = false;
 
@@ -38,7 +31,7 @@ public class PowerUpUntilPenalty extends UntypedActor {
 
     private FloatingHistory gyrozHistory = new FloatingHistory(8);
 
-    private ArrayList<Double> lastValues = new ArrayList<Double>();
+    private ArrayList<Double> lastGyrozValues = new ArrayList<>();
 
     enum SECTION {
         STILL_STANDING,
@@ -49,17 +42,17 @@ public class PowerUpUntilPenalty extends UntypedActor {
 
     /**
      * @param pilotActor The central pilot actor
-     * @param duration the period between two increases
+     * @param duration   the period between two increases
      * @return the actor props
      */
-    public static Props props( ActorRef pilotActor, int duration ) {
+    public static Props props(ActorRef pilotActor, int duration) {
         return Props.create(
-                PowerUpUntilPenalty.class, () -> new PowerUpUntilPenalty(pilotActor, duration ));
+                PowerUpUntilPenalty.class, () -> new PowerUpUntilPenalty(pilotActor, duration));
     }
+
     private final int duration;
 
     public PowerUpUntilPenalty(ActorRef pilotActor, int duration) {
-        lastIncrease = System.currentTimeMillis();
         this.kobayashi = pilotActor;
         this.duration = duration;
     }
@@ -68,13 +61,13 @@ public class PowerUpUntilPenalty extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
 
-        if ( message instanceof SensorEvent ) {
+        if (message instanceof SensorEvent) {
             handleSensorEvent((SensorEvent) message);
 
-        } else if ( message instanceof PenaltyMessage) {
-            handlePenaltyMessage ();
+        } else if (message instanceof PenaltyMessage) {
+            handlePenaltyMessage();
 
-        } else if ( message instanceof RaceStartMessage) {
+        } else if (message instanceof RaceStartMessage) {
             handleRaceStart();
 
         } else {
@@ -84,56 +77,60 @@ public class PowerUpUntilPenalty extends UntypedActor {
 
     private void handleRaceStart() {
         currentPower = 0;
-        lastIncrease = 0;
         gyrozHistory = new FloatingHistory(8);
         currentSection = SECTION.STILL_STANDING;
-        indexCompareTrack = 0;
         newSection = false;
         trackFound = false;
-        lastValues.clear();
+        lastGyrozValues.clear();
     }
 
     private void handlePenaltyMessage() {
         currentPower -= 10;
-        kobayashi.tell(new PowerAction((int)currentPower), getSelf());
+        kobayashi.tell(new PowerAction((int) currentPower), getSelf());
     }
 
-    private int nbLastValues = 4;
+    private int maxNbLastGyrozValues = 4;
 
     private boolean isLeftCurve(ArrayList<Double> lastValues) {
-        if (lastValues.size() < nbLastValues)
+        if (lastValues.size() < maxNbLastGyrozValues)
             return false;
 
-        for (Double f: lastValues) {
-            if (f >= -500) {
+        if (currentSection == SECTION.LEFT_CURVE || currentSection == SECTION.RIGHT_CURVE)
+            return false;
+
+        for (Double f : lastValues) {
+            if (f >= -500)
                 return false;
-            }
         }
 
         return true;
     }
 
     private boolean isRightCurve(ArrayList<Double> lastValues) {
-        if (lastValues.size() < nbLastValues)
+        if (lastValues.size() < maxNbLastGyrozValues)
             return false;
 
-        for (Double f: lastValues) {
-            if (f <= 500) {
+        if (currentSection == SECTION.RIGHT_CURVE || currentSection == SECTION.LEFT_CURVE)
+            return false;
+
+        for (Double f : lastValues) {
+            if (f <= 500)
                 return false;
-            }
         }
 
         return true;
     }
 
     private boolean isStraight(ArrayList<Double> lastValues) {
-        if (lastValues.size() < nbLastValues)
+        if (lastValues.size() < maxNbLastGyrozValues)
             return false;
 
-        for (Double f: lastValues) {
-            if (f < -500 || f > 500) {
+        if (currentSection == SECTION.STRAIGHT)
+            return false;
+
+        for (Double f : lastValues) {
+            if (f < -500 || f > 500)
                 return false;
-            }
         }
 
         return true;
@@ -143,75 +140,74 @@ public class PowerUpUntilPenalty extends UntypedActor {
     /**
      * Strategy: increase quickly when standing still to overcome haptic friction
      * then increase slowly. Probing phase will be ended by the first penalty
+     *
      * @param message the sensor event coming in
      */
     private void handleSensorEvent(SensorEvent message) {
 
         double gyrz = gyrozHistory.shift(message.getG()[2]);
-        if (lastValues.size() == nbLastValues) {
-            lastValues.remove(0);
+        if (lastGyrozValues.size() == maxNbLastGyrozValues) {
+            lastGyrozValues.remove(0);
         }
-        lastValues.add(gyrz);
-
-        show ((int)gyrz);
+        lastGyrozValues.add(gyrz);
+        show((int) gyrz);
 
         if (iAmStillStanding()) {
-            increase(0.5);
-        } else {
-            if (isLeftCurve(lastValues) && currentSection != SECTION.LEFT_CURVE && currentSection != SECTION.RIGHT_CURVE) {
-                currentSection = SECTION.LEFT_CURVE;
-                newSection = true;
-                System.out.println("Entering Left Curve");
-            } else if (isRightCurve(lastValues) && currentSection != SECTION.RIGHT_CURVE && currentSection != SECTION.LEFT_CURVE) {
-                currentSection = SECTION.RIGHT_CURVE;
-                newSection = true;
-                System.out.println("Entering Right curve");
-            } else if (isStraight(lastValues) && currentSection != SECTION.STRAIGHT){
-                currentSection = SECTION.STRAIGHT;
-                newSection = true;
-                System.out.println("Entering straight section");
+            increase(2);
+            kobayashi.tell(new PowerAction((int) currentPower), getSelf());
+            return;
+        }
+
+        if (isLeftCurve(lastGyrozValues)) {
+            currentSection = SECTION.LEFT_CURVE;
+            newSection = true;
+            System.out.println("Entering Left Curve");
+        } else if (isRightCurve(lastGyrozValues)) {
+            currentSection = SECTION.RIGHT_CURVE;
+            newSection = true;
+            System.out.println("Entering Right curve");
+        } else if (isStraight(lastGyrozValues)) {
+            currentSection = SECTION.STRAIGHT;
+            newSection = true;
+            System.out.println("Entering straight section");
+        }
+
+        if (newSection && !trackFound) {
+            newSection = false;
+
+            if (track.size() < 10) {
+                track.add(currentSection);
+                kobayashi.tell(new PowerAction((int) currentPower), getSelf());
+                return;
             }
 
-            if (newSection && !trackFound) {
-
-                if (track.size() < 9) {
+            do {
+                if (track.get(compareTrack.size()) == currentSection) {
+                    compareTrack.add(currentSection);
+                    break;
+                } else if (compareTrack.isEmpty()) {
                     track.add(currentSection);
+                    break;
                 } else {
                     do {
-                        if (track.get(compareTrack.size()) == currentSection) {
-                            compareTrack.add(currentSection);
-                            break;
-                        } else if (compareTrack.isEmpty()) {
-                            track.add(currentSection);
-                            break;
-                        } else {
-                            do  {
-                                track.add(compareTrack.remove(0));
-                            } while (!compareTrack.isEmpty() && !track.subList(0, compareTrack.size()).equals(compareTrack));
-                        }
-                    } while (true);
+                        track.add(compareTrack.remove(0));
+                    }
+                    while (!compareTrack.isEmpty() && !track.subList(0, compareTrack.size()).equals(compareTrack));
                 }
-                newSection = false;
+            } while (true);
 
-                if (track.equals(compareTrack)) {
-                    trackFound = true;
-                    System.out.println(compareTrack.toString());
-                }
-            /*} else if (newSection && trackFound){
-                checkTrack.add(currentSection);
-                if (!track.subList(0, checkTrack.size()).equals(checkTrack)) {
-
-                }
-                }*/
+            if (track.equals(compareTrack)) {
+                trackFound = true;
+                System.out.println(compareTrack.toString());
             }
         }
 
-        kobayashi.tell(new PowerAction((int)currentPower), getSelf());
+        kobayashi.tell(new PowerAction((int) currentPower), getSelf());
     }
 
-    private int increase ( double val ) {
+    private int increase(double val) {
         currentPower += val;
-        return (int)currentPower;
+        return (int) currentPower;
     }
 
     private boolean iAmStillStanding() {
@@ -219,7 +215,7 @@ public class PowerUpUntilPenalty extends UntypedActor {
     }
 
     private void show(int gyr2) {
-        int scale = 120 * (gyr2 - (-10000) ) / 20000;
+        int scale = 120 * (gyr2 - (-10000)) / 20000;
         System.out.println(StringUtils.repeat(" ", scale) + gyr2);
     }
 
